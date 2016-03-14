92a65a76ef7a453848ca5dc2045ac6fbd49adca2
Fernando Freire <ffreire@thoughtworks.com>
2016-03-14 21:56:49 +1100

    making the menu more complex

--- coffee-api.rb
+++ coffee-api.rb
@@ -5,11 +5,31 @@ get '/menu' do
   content_type :json
   status 200
   { coffees: [
-    {long_black: '/order/long-black'},
-    {flat_white: '/order/flat-white'},
-    {latte: '/order/latte'},
-    {espresso: '/order/espresso'},
-    {machiato: '/order/machiato'}]
+    {name: 'long black',
+     order_path: '/order/long-black',
+     price: 3,
+     caffeine_level: 8,
+     milk_ratio: 0},
+    {name: 'flat white',
+      order_path: '/order/flat-white',
+      price: 3.5,
+      caffeine_level: 5,
+      milk_ratio: 2},
+    {name: 'latte',
+      order_path: '/order/latte',
+      price: 3.5,
+      caffeine_level: 5,
+      milk_ratio: 3},
+    {name: 'espresso',
+      order_path: '/order/espresso',
+      price: 2,
+      caffeine_level: 10,
+      milk_ratio: 0},
+    {name: 'machiato',
+      order_path: '/order/machiato',
+      price: 2.5,
+      caffeine_level: 10,
+      milk_ratio: 0.5}]
   }.to_json
 end
 
