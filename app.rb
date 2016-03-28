0db98e1e270030c63a4410cbc4be0457d227545c
Armond Ave <aave@thoughtworks.com>
2016-03-28 20:11:56 +1100

    Temporarily moved classes to app.rb

--- app.rb
+++ app.rb
@@ -1,11 +1,62 @@
 # myapp.rb
 require 'sinatra'
 require 'json'
-require './validator.rb'
-require './inventory.rb'
-
 
 module CoffeeStore
+
+  class Validator
+
+    COFFEE_NAMES = ['long-black', 'flat-white', 'latte', 'espresso', 'machiato']
+
+    def is_valid_coffee_name(name)
+      COFFEE_NAMES.include? name.downcase
+    end
+
+    def is_valid_order_id(id)
+      id =~ /\d+/
+    end
+  end
+
+  class CoffeeInventory
+
+    def initialize()
+        @items = [
+          {
+            "name": "long black",
+            "order_path": "/order/long-black",
+            "price": 3,
+            "caffeine_level": 8,
+            "milk_ratio": 0
+          },
+          {
+            "name": "flat white",
+            "order_path": "/order/flat-white",
+            "price": 3.5,
+            "caffeine_level": 5,
+            "milk_ratio": 2
+          },
+          {
+            "name": "latte",
+            "order_path": "/order/latte",
+            "price": 3.5,
+            "caffeine_level": 5,
+            "milk_ratio": 3
+          },
+          {
+            "name": "machiato",
+            "order_path": "/order/machiato",
+            "price": 2.5,
+            "caffeine_level": 10,
+            "milk_ratio": 0.5
+          }
+        ]
+    end
+
+    def items
+    	@items
+	end
+  end
+  
   class Application < Sinatra::Base
 
     validator = Validator.new
