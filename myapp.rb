f8ad7e1a5be6369d6e0866e8f3a70c478ff870d5
Armond Ave <aave@thoughtworks.com>
2016-03-28 19:07:16 +1100

    Added endpoints for get-order-status and order-coffee

--- myapp.rb
+++ myapp.rb
@@ -4,7 +4,7 @@ require 'json'
 
 get '/menu' do
   content_type :json
-  return_message = {
+  response_body = {
       "coffees": [
         {
           "name": "long black",
@@ -35,6 +35,38 @@ get '/menu' do
           "milk_ratio": 0.5
         }
     ]
-	}
-  return_message.to_json;
+  }
+  response_body.to_json
+end
+
+get '/order/:id' do |id|
+  halt 400, "Invalid id" unless is_valid_order_id(id)
+  content_type :json
+  response_body = {
+  	"status": "READY"
+  }
+  response_body.to_json
+end
+
+post '/order/:coffee_name' do |coffee_name|
+   halt 400, "Coffee name not found" unless is_valid_coffee_name(coffee_name)
+
+   request_body = JSON.parse request.body.read
+   content_type :json
+
+   status 201
+   response_body = {
+     "order": "order/123",
+     "wait_time": 5
+   }
+
+   response_body.to_json
+end
+
+def is_valid_coffee_name(name)
+   ['long-black', 'flat-white', 'latte', 'espresso', 'machiato'].include? name.downcase
+end
+
+def is_valid_order_id(id)
+   id =~ /\d+/ 
 end
